898b0f836d3567c47962b608f236a773b9fb742b
Renaldi <renaldi.antonio19@gmail.com>
2024-10-01 14:54:24 +0700

    Initial Commit

---
+++ IndoorMappingNav/IndoorMappingNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndoorMappingNavApp.swift
+//  IndoorMappingNav
+//
+//  Created by Renaldi Antonio on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct IndoorMappingNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
