d1d091b30991631252e279e4bc8af1fe78a5cd04
Renaldi <renaldi.antonio19@gmail.com>
2024-10-04 14:28:11 +0700

    Some fixing (change scenekit to realitykit)

--- IndoorMappingNav/View/ContentView.swift
+++ IndoorMappingNav/View/ContentView.swift
@@ -6,14 +6,46 @@
 //
 
 import SwiftUI
+import RealityKit
+import MallMap
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            MallScene()
-                .ignoresSafeArea(.all)
+            Text("hai")
+            RealityView { content in
+                if let scene = try? await Entity(named: "Scene", in: mallMapBundle) {
+                    let scaleNum: Float = 0.05
+                    scene.transform.scale = [scaleNum, scaleNum, scaleNum]
+                    content.add(scene)
+                }
+            } update: { content in
+                
+            }
+            .gesture(
+                SpatialTapGesture()
+                    .targetedToAnyEntity()
+                    .onEnded({ target in
+                        let curTransform = target.entity.transform
+                        let curTranslation = curTransform.translation
+                        
+                        /// Koentji nya disini: relative to nil bakal refer scale entity ke world
+                        /// Jadi kita bisa samain gerakannya based on world's scale
+                        let entityScale = target.entity.scale(relativeTo: nil)
+                        let moveDistance: Float = 0.5
+                        let scaledMovement: Float = moveDistance / entityScale.z
+                        print(target.entity.scale(relativeTo: nil).z)
+                        
+                        var moveToLocation = curTransform
+                        moveToLocation.translation = simd_float3(x: curTranslation.x,
+                                                                 y: curTranslation.y,
+                                                                 z: curTranslation.z + scaledMovement)
+                        
+                        target.entity.move(to: moveToLocation, relativeTo: target.entity, duration: 0.5)
+                    })
+            )
+            .realityViewCameraControls(.orbit)
         }
-        .padding()
     }
 }
 
