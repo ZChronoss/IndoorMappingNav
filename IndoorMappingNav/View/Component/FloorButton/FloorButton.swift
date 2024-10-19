ea70b8a1aff61a73db264e009c1bf032572efbef
Renaldi <renaldi.antonio19@gmail.com>
2024-10-19 23:57:34 +0700

    Floor buttons done (yang kumpulan floor buttons)

--- IndoorMappingNav/View/Component/FloorButton/FloorButton.swift
+++ IndoorMappingNav/View/Component/FloorButton/FloorButton.swift
@@ -8,31 +8,38 @@
 import SwiftUI
 
 struct FloorButton: View {
-    @State var btnLbl: String = "GF"
+    @State var btnLbl: String = "1"
     @State private var didTap = false
     
     // TODO: bikin buat yang jumlah konten itu
     @State var contentNum: Int = 0
     
     
+    // Buat kasih tau active floor yang mana
+    @Binding var activeFloor: String
+    
     var body: some View {
         Button {
-            print("tits")
             didTap.toggle()
+            
+            // buat toggle floor yang mana juga
+            activeFloor = btnLbl
         } label: {
             // callout has the same property as floor button except its weight
             Text(btnLbl)
                 .font(.system(.callout, weight: .semibold))
+                .padding(12)
+                .frame(width: 50, height: 50)
         }
-        .padding(12)
-        .foregroundStyle(didTap ? .white : .secondaryAlt)
-        .background(didTap ? .blue600 : .backgroundSecondary)
+        .foregroundStyle(activeFloor == btnLbl ? .white : .secondaryAlt)
+        .background(activeFloor == btnLbl ? .blue600 : .backgroundSecondary)
         .clipShape(.circle)
         .frame(width: 50, height: 50)
+        .animation(.easeOut, value: didTap)
         .customBadge(5, color: .toilet)
     }
 }
 
 #Preview {
-    FloorButton()
+//    FloorButton()
 }
