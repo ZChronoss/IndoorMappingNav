dcf7006ca1abf4e7af7175b2d53aa7a491758f5c
ZChronoss <renaldi.antonio19@gmail.com>
2024-11-06 00:17:25 +0700

    feat: search function and its flow worked

--- IndoorMappingNav/ViewModel/StoreDetailView-ViewModel.swift
+++ IndoorMappingNav/ViewModel/StoreDetailView-ViewModel.swift
@@ -9,30 +9,20 @@ import Foundation
 
 extension StoreDetailView {
     
+    @MainActor
     class ViewModel: ObservableObject {
         private let cloudkitController = CloudKitController()
+        @Published var storeName: String = ""
         @Published var stores: [Store] = []
         
         @Published var store: Store = Store()
         @Published private(set) var isLoading = false
         
-        func getStores() async {
+        func getStoreDetail() async {
             isLoading = true
             
             do {
-                self.stores = try await cloudkitController.fetchStores()
-            } catch {
-                print("Error: Data fetching failed (\(error.localizedDescription))")
-            }
-            
-            isLoading = false
-        }
-        
-        func getStoreDetail(_ name: String) async {
-            isLoading = true
-            
-            do {
-                self.store = try await cloudkitController.fetchStoreByName(name: name)
+                self.store = try await cloudkitController.fetchStoreByName(name: storeName)
             } catch {
                 print("Error: Data fetching failed (\(error.localizedDescription))")
             }
